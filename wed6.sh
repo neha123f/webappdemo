05cb2d4d35512b80616c29565fd87776d6cfd885
neha123f <121794908+neha123f@users.noreply.github.com>
2023-09-14 10:33:44 +0530

    Update wed6.sh

--- wed6.sh
+++ wed6.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
 
-# Check if the command-line argument for the end folder is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <end_folder>"
-  exit 1
-fi
+# Prompt the user for a folder number
+read -p "Enter folder number: " folder_number
 
-# Define the end folder from the command-line argument
-end_folder="$1"
-
-# Check if the end folder exists
-if [ ! -d "$end_folder" ]; then
-  echo "End folder $end_folder does not exist. Exiting the script now."
-  exit 1
-fi
-
-# Function to list contents of a folder and its parent folders recursively
-list_contents_recursive() {
-  local folder="$1"
+# Check if the folder with the specified number exists
+if [ -d "$folder_number" ]; then
+  # Display the contents of the specified folder
+  echo "Contents of Folder $folder_number:"
+  ls "$folder_number"
   
-  echo "Contents of Folder $folder:"
-  ls "$folder"
+  # Get the parent directory of the specified folder
+  parent_directory="$(dirname "$folder_number")"
   
-  # Check if we've reached the root directory
-  if [ "$folder" != "/" ]; then
-    list_contents_recursive "$(dirname "$folder")"
-  fi
-}
-
-# Call the recursive function to list contents
-list_contents_recursive "$end_folder"
+  # Display the contents of the folder above it (parent directory)
+  echo "Contents of Folder $parent_directory:"
+  ls "$parent_directory"
+else
+  echo "Folder $folder_number does not exist."
+fi
