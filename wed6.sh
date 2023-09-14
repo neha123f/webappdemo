ee48501d8df49d2c728ab97760efae6de5bc0102
neha123f <121794908+neha123f@users.noreply.github.com>
2023-09-14 12:16:43 +0530

    Update wed6.sh

--- wed6.sh
+++ wed6.sh
@@ -4,64 +4,28 @@
 current_directory=$(pwd)
 
 # List the contents of the current directory
-echo "Contents of Current Directory ($current_directory):"
-ls "$current_directory"
+current_directory_contents=$(ls "$current_directory")
 
 # Ask the user for input
-#read -p "Enter a folder name: " user_input
-user_input=$1
+read -p "Enter a folder name: " user_input
+
 # Check if the user input folder exists in the current directory
 if [ -d "$current_directory/$user_input" ]; then
+  found=false
   # Loop through and print the contents of folders until the user's input folder
   for folder in "$current_directory"/*; do
     if [ -d "$folder" ]; then
-      echo "Contents of Folder $folder:"
-      ls "$folder"
       if [ "$folder" == "$current_directory/$user_input" ]; then
-        break
+        found=true
+      elif [ "$found" == "true" ]; then
+        echo "Contents of Folder $folder:"
+        ls "$folder"
       fi
     fi
   done
+  if [ "$found" == "false" ]; then
+    echo "Folder $user_input not found."
+  fi
 else
   echo "Folder $user_input does not exist in the current directory."
-fi
-#!/bin/bash
-
-# Check if the command-line argument for the end folder is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <end_folder>"
-  exit 1
-fi
-
-# Define the end folder from the command-line argument
-end_folder="$1"
-
-# Check if the end folder exists
-if [ ! -d "$end_folder" ]; then
-  echo "End folder $end_folder does not exist. Exiting the script now."
-  exit 1
-fi
-
-# Function to list contents of a folder
-list_contents() {
-  local folder="$1"
-
-  # List the contents of the specified folder
-  if [ -d "$folder" ]; then
-    echo "Contents of Folder $folder:"
-    ls "$folder"
-  else
-    echo "Folder $folder does not exist."
-  fi
-}
-
-# List contents of the specified folder
-list_contents "$PWD/$end_folder"
-
-# Get the parent folder (one level above)
-parent_folder=$(dirname "$PWD/$end_folder")
-
-# List contents of the parent folder if it's not the root directory
-if [ "$parent_folder" != "/" ]; then
-  list_contents "$parent_folder"
 fi
