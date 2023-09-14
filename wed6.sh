7404457064e5fa8414dede8df80f6f3237e09167
neha123f <121794908+neha123f@users.noreply.github.com>
2023-09-14 10:59:18 +0530

    Update wed6.sh

--- wed6.sh
+++ wed6.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Prompt the user for a folder number
-#read -p "Enter folder number: " folder_number
-folder_number=$1
 # Function to list contents of a folder and its parent folders recursively
 list_contents_recursive() {
   local folder="$1"
@@ -10,37 +7,22 @@ list_contents_recursive() {
   echo "Contents of Folder $folder:"
   ls "$folder"
   
-  # Check if we've reached the root directory
-  if [ "$folder" != "/" ]; then
+  # Check if we've reached the current working directory
+  if [ "$folder" != "$(pwd)" ]; then
     list_contents_recursive "$(dirname "$folder")"
   fi
 }
 
-# Create a temporary file for the directory structure
-tempfile=$(mktemp)
+# Get the current working directory
+current_directory="$(pwd)"
 
-# Find all directories in the current directory and its subdirectories
-find . -type d -print | sed 's#^\./##' | sort > "$tempfile"
+# Prompt the user for a folder number
+read -p "Enter folder number: " folder_number
 
 # Check if the folder number is valid
-if ! grep -q "^$folder_number$" "$tempfile"; then
+if [ -d "$folder_number" ]; then
+  # Display the contents of the specified folder
+  list_contents_recursive "$folder_number"
+else
   echo "Folder $folder_number does not exist."
-  rm "$tempfile"
-  exit 1
 fi
-
-# Read the directory structure from the temporary file
-found=false
-
-while read -r line; do
-  if [ "$line" = "$folder_number" ]; then
-    found=true
-  fi
-  
-  if "$found"; then
-    list_contents_recursive "$line"
-  fi
-done < "$tempfile"
-
-# Remove the temporary file
-rm "$tempfile"
